e6600e457eda9de686b300eaf822eac683595db3
Randolph Amaya <randolphjr7@gmail.com>
2019-07-20 11:39:56 -0400

    fixed buggy start-button, implemented gravity and collision, changed images

--- leaf3.js
+++ leaf3.js
@@ -3,156 +3,309 @@
   const myCanvas = document.getElementById("the-canvas");
   const ctx = myCanvas.getContext("2d");
 
-  
-  class Game {
+  /////////////////////////////////////////////////////////////////
+  /////////////////// Classes Section /////////////////////////////
+  /////////////////////////////////////////////////////////////////
 
-    constructor(){
-      this.leaf = {}; // leaf => object
-      this.skyscrapers = [];
-      this.birds = [];
-      this.score = 0;
-    }
-  }
-
+  /////////////////////the leaf class//////////////////////////////
   class Leaf {
     
-    constructor(name,x,y,width,height){
-      this.name = name;
-      this.x = x;
-      this.y = y;
-      this.width = width;
-      this.height = height;
-      this.img = "./images/leaf1.png";
+    constructor(){
+      this.x = 15;
+      this.y = 250;
+      this.width = 50;
+      this.height = 80;
+      this.img = "./images/mari2.png";
       this.speedX = 0;
       this.speedY = 0;
       this.gravity = 0.5;
       this.gravitySpeed = 0;
-      this.bottom = 700;
+      this.bottom = myCanvas.height;
+      this.top = 0;
     }
-  
-    // draws the leaf
+    
+    // draw the leaf
     drawLeaf(){
       const leafImg = new Image();
       leafImg.src = this.img;
       ctx.drawImage(leafImg, this.x, this.y, this.width, this.height);
     }
-
+    
+    // gravity stuff
     gravityEffect(){
+        this.hitTop();
       this.gravitySpeed += this.gravity;
       this.y += this.speedY + this.gravitySpeed;
       this.hitBottom();
+      
     }
-
+    
+    // check if it hits bottom
     hitBottom() {
       if(this.y > (this.bottom - this.height)){
         this.y = (this.bottom - this.height);
         this.gravitySpeed = 0;
       }
     }
-
+    
+    // check if it hits top
+    hitTop(){
+      if(this.y < (5)){
+        this.y = (5);
+        return true;
+      }
+      return false;
+    }
+    
+    // recieve -5 on keydown, recieve .2 on keyup
     accelerate(n){
       this.gravity = n;
     }
 
+    leafVertexXcoord(){
+      return this.x + (.5 * this.width);
+    }
 
+    leafVertexYcoord(){
+      return this.y + (.5 * this.height);
+    }
+    
   }
-
+ 
+//////////////////////////////the obstacles class///////////////////
   class Obstacles {
-
-    constructor(x,y,width, height){
+    
+    constructor(x,y,width,height){
       this.x = x;
       this.y = y;
       this.width = width;
       this.height = height;
     }
-
+    
+    // draw the obstacle
     drawObstacles(){
       ctx.fillStyle = "red";
       ctx.fillRect(this.x, this.y, this.width, this.height);
     }
+    
+    // move obstacle left
+    moveLeft(){
+      this.x -= 10; 
+    }
 
+    obstacleVertexXcoord(){
+      return this.x + (.5 * this.width);
+    }
+
+    obstacleVertexYcoord(){
+      return this.y + (.5 * this.height);
+    }
+
+  }
+  
+////////////////////////the building class////////////////////////////
+  class buildings {
+
+    constructor(x,y,width,height, image){
+      this.x = 850;
+      this.y = y;
+      this.width = width;
+      this.height = height;
+      this.img = image;
+    }
+
+    // draw building
+    drawBuilding(){
+      const buildingImg = new Image();
+      buildingImg.src = this.img;
+      ctx.drawImage(buildingImg, this.x, this.y, this.width, this.height);
+    }
+
+    // move building left
     moveLeft(){
       this.x -= 10;
     }
   }
 
+//////////////////////////the bird class////////////////////////////
+  class Bird {
     
-      // listens for keystrokes
-  document.onkeydown = function (e) {
-    console.log(`what is e: `, e);
+    constructor(y,width, height){
+      this.x = myCanvas.width + 200;
+      this.y = y;
+      this.width = 80;
+      this.height = 80;
+      this.img = "./images/funnyRaven.png";
+    }
     
-    let up = e.keyCode;
-    let jump = -.05;
-    switch(up){
-      case 32:
-        currentLeaf.accelerate(jump);
-        break;
-      default:
-        console.log("default reached");
+    // draw the bird
+    drawBird(){
+      const birdImg = new Image();
+      birdImg.src = this.img;
+      ctx.drawImage(birdImg, this.x, this.y, this.width, this.height);
+    }
+    
+    // move the bird left
+    moveLeft(){
+      this.x -= 10;
+    }
+  }           
+
+////////////////////////////the Game class/////////////////////////////
+  class Game {
+
+    constructor(){
+      this.leaf = {}; // leaf => object
+      this.skyscrapers = [];
+      this.buildings = [];
+      this.birds = [];
+      this.score = 0;
     }
   }
 
-
+  // listen for keystrokes
+  document.onkeydown = function (e) {
+    
+    const up = e.keyCode;
+    const jump = -.5;
+    
+    if(up ===32){
+      // if currentLeaf has not hit the top then jump
+      if(!currentLeaf.hitTop()){
+        currentLeaf.accelerate(jump);
+      }
+      
+    }
+  }
+  
+  // when there are no keystrokes
   document.onkeyup = function () {
     let down = .2;
     currentLeaf.accelerate(down);
   }
 
-  
-
-  const currentGame = new Game();
-  const currentLeaf = new Leaf("anthony", 100,250,50,80);
-  currentGame.leaf = currentLeaf;
-
-
- /*    document.getElementById("start-button").onclick = function() {
-    startGame();
+  /////////////////////////////////add score/////////////////////////////
+  let newScore = 0;
+  function addScore(){
+    newScore++;
   }
-
-  function startGame(){
-
-    console.log(`startGame() invoked`);
-    console.log();
-    document.getElementById("game-board").style.display = "block";
-
   
-    //currentLeaf.drawLeaf();
-
-
-    refresh();
-  } */
   
-
+  // Create objects/instances of the classes
+  const currentGame = new Game();
+  const currentLeaf = new Leaf();
+  currentGame.leaf = currentLeaf;
+  
+  
+  ///////////////////////////the animation loop//////////////////////////
   let frames = 0;
   function refresh(){
-
-    ctx.clearRect(0,0,800,700);
+    
+    // clear canvas before drawing
+    ctx.clearRect(0,0,myCanvas.width,myCanvas.height);
+    // draw leaf
     currentLeaf.drawLeaf();
+    // apply gravity
     currentLeaf.gravityEffect();
-    console.log("name is: ", currentLeaf.name);
-    //currentLeaf.gravityEffect();
 
     frames++;
-
-     if(frames % 100 === 1){
-      randomObstacleX = myCanvas.width;
-      obstacleY = Math.floor(Math.random() * 600);
-      randomObstacleWidth = Math.floor(Math.random() * 50) + 20;
-      randomObstacleHeight = Math.floor(Math.random() * 50) + 20;
-      let obstacle = new Obstacles (randomObstacleX, obstacleY, randomObstacleWidth, randomObstacleHeight);
-      currentGame.skyscrapers.push(obstacle);
+    
+    if(frames % 150 === 1){
+      
+      // create bird object
+      randomBirdY = Math.floor(Math.random() * 650);
+      let currentBird = new Bird(randomBirdY);
+      currentGame.birds.push(currentBird);
+      
+      
     }
-
+    
+    if (frames % 100 === 1){
+      
+      // create min & max for specific obstacle range
+      let min = 500;
+      let max = 525;
+      
+      // randomize parameters for obstacle object 
+      obstacleX = myCanvas.width;
+      let randomY = Math.floor(Math.random() * (max - min + 1)) + min;
+      
+      obstacleWidth = 100;
+      obstacleHeight = myCanvas.height - randomY;
+      
+      let obstacle = new Obstacles (obstacleX, randomY, obstacleWidth, obstacleHeight); 
+      
+      // push array
+      currentGame.skyscrapers.push(obstacle);
+      
+    }
+    
+    
+    // iterate through the array of skycrapers; move objects/skyscrapers left and draw
     for(let i = 0; i < currentGame.skyscrapers.length; i++){
       
       currentGame.skyscrapers[i].moveLeft();
-      currentGame.skyscrapers[i].drawObstacles();
-    } 
+      
+      //////////////////////////detect collision/////////////////////////////
+  
+        const leafVertexX = currentLeaf.leafVertexXcoord();
+        const leafVertexY = currentLeaf.leafVertexYcoord();
+        const obstacleVertexX = currentGame.skyscrapers[i].obstacleVertexXcoord();
+        const obstacleVertxY = currentGame.skyscrapers[i].obstacleVertexYcoord();
+  
+        let vertexWidth = Math.abs(leafVertexX - obstacleVertexX).toFixed(2);
+        let vertexHeight = Math.abs(leafVertexY - obstacleVertxY).toFixed(2);
 
+        console.log("leaf vertex coordinates are: (", leafVertexX, ",", leafVertexY,")");
+        console.log("obstacle vertex coordinates are: (", obstacleVertexX, ",", obstacleVertxY,")");
+        console.log("vertex width: ", vertexWidth, " and vertex height is: ", vertexHeight)
+  
+        function xCollision(){
+          if(vertexWidth < ((.5 * currentLeaf.width) + (.5 * currentGame.skyscrapers[i].width))){
+            console.log("X collission!");
+            return true;
+          }
+          return false;
+        }
+          
+        function yCollision(){
+          
+          if(vertexHeight < ((.5 * currentLeaf.height) + (.5 * currentGame.skyscrapers[i].height))){
+            console.log("Y collision!");
+            return true;
+          } 
+          return false;
+        }
+
+        if((xCollision()) && (yCollision())){
+          console.log("BOOOOOOOOOM!!!!");
+        }
+        
+      ////////////////////////////////////////////////////////////////////////////////
+
+      currentGame.skyscrapers[i].drawObstacles();
+
+      if(currentGame.skyscrapers[i].x + currentGame.skyscrapers[i].width < 0){
+
+        currentGame.skyscrapers.shift();
+      }
+    }
+
+    
+    // iterate through the array of birds; move objects/birds left and draw
+    for(let k = 0; k < currentGame.birds.length; k++){
+
+      currentGame.birds[k].moveLeft();
+      currentGame.birds[k].drawBird();
+    }  
+    
+    // loop again
     requestAnimationFrame(refresh);
 
   }
 
+
+  // start the recursive function refresh()
   refresh();
 
 
